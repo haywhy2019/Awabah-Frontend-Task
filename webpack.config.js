a8ba90376ea92dd917b1c963495c1e37b9aa4af0
flint <olusegun.sola19@gmail.com>
2022-02-20 20:10:10 +0100

    edited package

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,16 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images/'
+            }
+          }
+        ]
+      
       },
     ],
   },
