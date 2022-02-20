c99f8c2a336a5d2e15fb7a057f146c54dd0a56ee
flint <olusegun.sola19@gmail.com>
2022-02-20 19:52:16 +0100

    added image

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,10 @@ module.exports = {
         test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
         use: ["file-loader"],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   plugins: [
